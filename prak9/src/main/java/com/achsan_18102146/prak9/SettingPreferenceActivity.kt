9b1fe3d64875667162dc66a44c9a11bcc07a5b7e
localuser <achsanaris05@gmai.com>
2021-01-08 14:48:20 +0700

    create setting preference.kt

---
+++ prak9/src/main/java/com/achsan_18102146/prak9/SettingPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.achsan_18102146.prak9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_preference)
+    }
+}
